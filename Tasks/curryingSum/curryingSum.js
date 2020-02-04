2fcffc2e7ccb2c2d2d2ee7ad479bab0befef2af5
Dima <onridge7311@gmail.com>
2020-02-04 10:49:36 +0200

    Currying Sum

---
+++ Tasks/curryingSum/curryingSum.js
@@ -0,0 +1,15 @@
+function curryingSum(num) {
+    let result = num;
+
+    function secondFn(num2) {
+        result += num2;
+        return secondFn;
+    }
+
+    secondFn.toString = function() {
+        return result;
+    };
+    return secondFn;
+}
+
+module.exports = curryingSum;
